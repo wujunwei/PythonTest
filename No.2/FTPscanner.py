dca367e1b605b5be5b02b62895fae98ecd6a0e62
adam123 <wjw3323@live.com>
2016-08-08 14:37:40 +0800

    alter username

--- No.2/FTPscanner.py
+++ No.2/FTPscanner.py
@@ -1,16 +1,18 @@
 import ftplib
 
+# 公司97服务器
+
 
 def anonLogin(hostname):
     try:
         ftp = ftplib.FTP(hostname)
-        ftp.login('ubuntu', 'WUJUNWEI123')
+        ftp.login('anonymous', 'sdfjafj@qq.com')
         print('\n[*]' + str(hostname) + ' FTP Anonymous Login succeed')
         return True
     except Exception as e:
         print(e)
         return False
 
-host = '115.159.222.116'
+host = '192.168.1.97'
 anonLogin(host)
 
