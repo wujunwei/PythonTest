5c54f9568a820788cc6603a053caf56982f18f80
adam123 <wjw3323@live.com>
2016-08-10 17:57:29 +0800

    add No.5

---
+++ No.5/testwireless.py
@@ -0,0 +1,33 @@
+from winreg import *
+
+
+def val2addr(val):
+    addr = ''
+    for ch in val:
+        addr += '%02x ' % ord(ch)
+    addr = addr.strip(' ').replace(' ', ':')[0:17]
+    return addr
+
+
+def printNets():
+    net = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetWorkList\Signatures\Unmanaged"
+    key = OpenKey(HKEY_LOCAL_MACHINE, net)
+    print("\n [*] Networks You have Joined")
+    for i in range(100):
+        try:
+            guid = EnumKey(key, i)
+            netkey = OpenKey(key, str(guid))
+            (n, addr, t) = EnumValue(netkey, 5)
+            (n, name, t) = EnumValue(netkey, 4)
+            macAddr = val2addr(addr)
+            netName = str(name)
+            print('[+] ' + netName + " " + macAddr)
+            CloseKey(netkey)
+        except:
+            break
+
+
+def main():
+    printNets()
+if __name__ == '__main__':
+    main()
