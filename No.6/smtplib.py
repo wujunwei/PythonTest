fd9cdd748c925dc018efba2bc72dcb8ab4be3976
adam123 <wjw3323@live.com>
2016-08-10 11:50:00 +0800

    add smtp test

---
+++ No.6/smtplib.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def sendMail(user, pwd, to, subject, text):
+    msg = MIMEText(text)
+    msg['From'] = user
+    msg['To'] = to
+    msg['Subject'] = subject
+    try:
+        smtpServer = smtplib.SMTP('smtp.163.com', 57)
+        smtpServer.set_debuglevel(1)
+        print("[+] Connecting!")
+        smtpServer.ehlo()
+        print("[+] start encrypted")
+        smtpServer.starttls()
+        smtpServer.ehlo()
+        print("[+] Logging Into Mail Server")
+        smtpServer.login(user, pwd)
+        print("[+] sending Mail")
+        smtpServer.sendmail(user, to, msg.as_string())
+        smtpServer.close()
+    except Exception as e:
+        print("[-] " + e)
+
+From = "15651757672@163.com"
+password = "QWERTYUIOP123"
+sendMail(From, password, From, 'Re: Important', 'Test Server')
+
