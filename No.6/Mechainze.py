e963b9d2de596bfdfe7306a206bab3cdee6f283c
adam123 <wjw3323@live.com>
2016-08-10 10:09:48 +0800

    alter url

--- No.6/Mechainze.py
+++ No.6/Mechainze.py
@@ -2,6 +2,8 @@ import mechanize
 
 
 # test page
+
+
 # def viewPage(url):
 #     browser = mechanize.Browser()
 #     page = browser.open(url)
@@ -12,14 +14,28 @@ import mechanize
 # test proxy
 
 
-def testProxy(url, proxy):
+# def testProxy(url, proxy):
+#     browser = mechanize.Browser()
+#     browser.set_proxies(proxy)
+#     page = browser.open(url)
+#     source_code = page.read()
+#     print(source_code)
+#
+#
+# ur = 'http://www.baidu.com/'
+# hideMeProxy = {'http': '216.155.139.115:80'}
+# testProxy(ur, hideMeProxy)
+
+
+# test useragent
+
+
+def testUserAgent(url, userAgent):
     browser = mechanize.Browser()
-    browser.set_proxies(proxy)
+    browser.addheaders = userAgent
     page = browser.open(url)
     source_code = page.read()
     print(source_code)
-
-
-ur = 'http://www.baidu.com/'
-hideMeProxy = {'http': '216.155.139.115:3128'}
-testProxy(ur, hideMeProxy)
+url = 'http://www.baidu.com/'
+userAgent = [('User-agent', 'Mozilla/5.0 (X11; U; ' + 'Linux 2.4.2-2 i586; en-us; m18) Gecko/20010131 Netscape6/6.01')]
+testUserAgent(url, userAgent)
