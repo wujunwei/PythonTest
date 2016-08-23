f7dcaba4849fa1577a5bf87d1a6c195bfa3d3c4e
wujunwei <wjw3323@live.com>
2016-08-23 23:15:02 +0800

    add tkinter model

--- tkinter_test/tk_test.py
+++ tkinter_test/tk_test.py
@@ -3,12 +3,12 @@ import tkinter as tk
 
 def say_hi(var):
     text = var.get()
-    var.set("hello"+text)
+    var.set("hello" + text)
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        super().__init__(master,)
+        super().__init__(master, )
         self.quit = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
         self.hi_there = tk.Button(self)
         self.text = tk.StringVar()
@@ -20,7 +20,7 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = lambda : say_hi(self.text)
+        self.hi_there["command"] = lambda: say_hi(self.text)
         self.hi_there.pack(side="top")
 
         self.quit.pack(side="bottom")
