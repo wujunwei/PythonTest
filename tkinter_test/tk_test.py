1341ae0405fe36cd11123ed2fb8ecc13415f18c2
wujunwei <wjw3323@live.com>
2016-08-26 22:11:08 +0800

    add tkinter model

--- tkinter_test/tk_test.py
+++ tkinter_test/tk_test.py
@@ -2,31 +2,25 @@ import tkinter as tk
 
 
 def say_hi(var):
-    text = var.get()
-    var.set("hello" + text)
-
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master, )
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
-        self.hi_there = tk.Button(self)
-        self.text = tk.StringVar()
-        self.input = tk.Entry(self, width=21)
-        self.input['textvariable'] = self.text
-        self.input.pack(side="bottom")
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = lambda: say_hi(self.text)
-        self.hi_there.pack(side="top")
-
-        self.quit.pack(side="bottom")
+    tex = var.get()
+    var.set("hello" + tex)
 
 
 root = tk.Tk()
-app = Application(root)
+root.title("test tkinter")
+root.geometry('300x300')  # 是x 不是*
+root.resizable(width=True, height=True)  # 宽不可变, 高可变,默认为T
+app = tk.Frame(root)
+quitButton = tk.Button(app, text="QUIT", fg="red", command=root.destroy)
+hi_there = tk.Button(app)
+text = tk.StringVar()
+inputButton = tk.Entry(app, width=21)
+inputButton['textvariable'] = text
+inputButton.pack(side="bottom")
 
+hi_there["text"] = "Hello World\n(click me)"
+hi_there["command"] = lambda: say_hi(text)
+hi_there.pack(side="left")
+quitButton.pack(side="bottom")
+app.pack()
 app.mainloop()
