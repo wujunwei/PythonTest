5279d052c9f1109028e357021fb4a652a0e287a2
adam123 <wjw3323@live.com>
2016-08-29 15:20:37 +0800

    add 初始化

--- tkinter_test/UnixTime.py
+++ tkinter_test/UnixTime.py
@@ -2,8 +2,11 @@ from tkinter import *
 from datetime import *
 import time
 
+DEFAULTDATE = "0000-01-01 00:00:00"
+
 
 def change_unix_to_date(int_date, view_text):
+
     int_time = int_date.get()
     value = time.localtime(int_time)
     dt = time.strftime("%Y-%m-%d %H:%M:%S", value)
@@ -12,6 +15,8 @@ def change_unix_to_date(int_date, view_text):
 
 def change_date_to_unix(int_date, view_text):
     time_str = view_text.get()
+    time_str += DEFAULTDATE[len(time_str):]
+    view_text.set(time_str)
     value = time.strptime(time_str, '%Y-%m-%d %H:%M:%S')
     value = time.mktime(value)
     int_date.set(int(value))
@@ -36,6 +41,9 @@ inputBox.pack()
 # button
 change_int = Button(mainFrame, text="时间戳转string", command=lambda: change_unix_to_date(Unix, result))
 change_int.pack(side=LEFT)
+
+hide_btn = Button(mainFrame, text="隐藏", command=root.withdraw)
+hide_btn.pack(side=BOTTOM)
 
 change_str = Button(mainFrame, text="string转时间戳", command=lambda: change_date_to_unix(Unix, result))
 change_str.pack(side=RIGHT)
