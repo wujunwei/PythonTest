61d6cc6f3da3ce00c1184546d8ccb2284a464cf5
adam123 <wjw3323@live.com>
2017-01-19 16:26:19 +0800

    添加强制关机小程序

--- tkinter_test/schedule_shutdown.py
+++ tkinter_test/schedule_shutdown.py
@@ -1,21 +1,28 @@
+# coding=gbk
 import os
 import tkinter as tk
 from autopy3 import alert
+import datetime
 
 
 def exc(command):
     try:
         os.system(command)
     except:
-        alert.alert(msg="æœªçŸ¥é”™è¯¯ï¼Œæ“ä½œå¤±è´¥ï¼Œè¯·æ£€æŸ¥æƒé™!!", title="Alert", default_button="OK")
+        alert.alert(msg="Î´Öª´íÎó£¬²Ù×÷Ê§°Ü£¬Çë¼ì²éÈ¨ÏŞ!!", title="Alert", default_button="OK")
         return False
     return True
 
 
 def shutdown(hours, mins, second, is_force):
-    hours = hours.get()
-    mins = mins.get() + 60 * hours
-    second = second.get() + 60 * mins
+    # hours = hours.get()
+    # mins = mins.get() + 60 * hours
+    # second = second.get() + 60 * mins
+    now = datetime.datetime.now()
+    d1 = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, now.second)
+    d2 = datetime.datetime(now.year, now.month, now.day, hours.get(), mins.get(), second.get())
+    second = (d2 - d1).seconds
+    # alert.alert("{0} {1} Ïà²î {2} Ãë".format(d1, d2, str(second)))
     if is_force.get():
         command = "shutdown -s -f -t " + str(second)
     else:
@@ -23,20 +30,20 @@ def shutdown(hours, mins, second, is_force):
     exc(command)
 
 root = tk.Tk()
-root.title("è®¡åˆ’å…³æœºå°å·¥å…·")
-root.geometry('210x160')  # æ˜¯x ä¸æ˜¯*
-root.resizable(width=True, height=True)  # å®½ä¸å¯å˜, é«˜å¯å˜,é»˜è®¤ä¸ºT
+root.title("¼Æ»®¹Ø»úĞ¡¹¤¾ß")
+root.geometry('210x160')  # ÊÇx ²»ÊÇ*
+root.resizable(width=True, height=True)  # ¿í²»¿É±ä, ¸ß¿É±ä,Ä¬ÈÏÎªT
 root.wm_attributes('-topmost', 1)
-tk.Label(root, text="è¯·å¡«å†™å…³æœºæ—¶é—´").pack(side="top")
+tk.Label(root, text="ÇëÌîĞ´¹Ø»úÊ±¼ä").pack(side="top")
 Hours = tk.IntVar()
 Mins = tk.IntVar()
 Seconds = tk.IntVar()
 isForce = tk.IntVar()
-cb = tk.Checkbutton(root, text="æ˜¯å¦å¼ºåˆ¶å…³æœº", variable=isForce, onvalue=1)
+cb = tk.Checkbutton(root, text="ÊÇ·ñÇ¿ÖÆ¹Ø»ú", variable=isForce, onvalue=1)
 cb.pack()
 
 app1 = tk.Frame(root)
-label = tk.Label(app1, text="å°æ—¶:")
+label = tk.Label(app1, text="Ğ¡Ê±:")
 label.pack(side="left")
 inputHour = tk.Entry(app1)
 inputHour['textvariable'] = Hours
@@ -44,7 +51,7 @@ inputHour.pack(side='right')
 app1.pack(side=tk.TOP)
 
 app2 = tk.Frame(root)
-label = tk.Label(app2, text="åˆ†é’Ÿ:")
+label = tk.Label(app2, text="·ÖÖÓ:")
 label.pack(side="left")
 inputMin = tk.Entry(app2)
 inputMin['textvariable'] = Mins
@@ -52,7 +59,7 @@ inputMin.pack()
 app2.pack(side=tk.TOP)
 
 app3 = tk.Frame(root)
-label = tk.Label(app3, text="  ç§’:")
+label = tk.Label(app3, text="  Ãë:")
 label.pack(side="left")
 inputSec = tk.Entry(app3)
 inputSec['textvariable'] = Seconds
@@ -61,11 +68,11 @@ app3.pack()
 
 app4 = tk.Frame(root)
 shutdown_btn = tk.Button(app4)
-shutdown_btn["text"] = "å…³æœº"
+shutdown_btn["text"] = "¹Ø»ú"
 shutdown_btn["command"] = lambda: shutdown(Hours, Mins, Seconds, isForce)
 shutdown_btn.pack(side="left")
 cancel_btn = tk.Button(app4)
-cancel_btn["text"] = "å–æ¶ˆå…³æœº"
+cancel_btn["text"] = "È¡Ïû¹Ø»ú"
 cancel_btn["command"] = lambda: exc("shutdown -a")
 cancel_btn.pack(side="right")
 app4.pack(side="bottom")
